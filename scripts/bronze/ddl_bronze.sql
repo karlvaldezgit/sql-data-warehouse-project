20cbee1c37cfadc6908ad0218fe6656ad1e5ed12
karlvaldezgit <karlvaldez49@yahoo.com>
2026-07-10 15:17:01 +0800

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,1 +1,76 @@
+-- CREATING Bronze crm files
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info (
+cst_id INTEGER,
+cst_key VARCHAR(50),
+cst_firstname VARCHAR(50),
+cst_lastname VARCHAR(50),
+cst_marital_status VARCHAR(50),
+cst_gndr VARCHAR(50),
+cst_create_date DATE
+);
 
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+prd_id INTEGER,
+prd_key VARCHAR(50),
+prd_nm VARCHAR(50),
+prd_cost INT,
+prd_line VARCHAR(50),
+prd_start_dt DATE,
+prd_end_dt DATE
+);
+
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+sls_ord_num VARCHAR(50),
+sls_prd_key VARCHAR(50),
+sls_cust_id INT,
+sls_order_dt DATE,
+sls_ship_dt DATE,
+sls_due_dt DATE,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+
+
+-- CREATING Bronze erp files
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+cid VARCHAR(50),
+bdate DATE, 
+gen VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+cid VARCHAR(50),
+cntry VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+id VARCHAR(50),
+cat VARCHAR(50),
+subcat VARCHAR(50),
+maintenance VARCHAR(50)
+);
+
+
+--Deleting all table rows if exists before importing. Do this with all other files.
+TRUNCATE TABLE bronze.crm_cust_info;
+
+CSV files were imported using pgAdmin:
+1. Right-click the table
+2. Select Import/Export Data
+3. Choose the CSV file
+4. Set Format = CSV
+5. Set Header = Yes
+6. Click Import
+
+-- If encounters importing errors. Alter columns to text.
+ALTER TABLE bronze.crm_sales_details
+ALTER COLUMN sls_order_dt TYPE TEXT,
+ALTER COLUMN sls_ship_dt TYPE TEXT,
+ALTER COLUMN sls_due_dt TYPE TEXT;
