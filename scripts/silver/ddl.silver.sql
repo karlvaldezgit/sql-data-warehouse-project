de4d11f3a13f239048d5504c72c9e47d235428cc
karlvaldezgit <karlvaldez49@yahoo.com>
2026-07-12 23:20:02 +0800

    Update ddl.silver.sql

--- scripts/silver/ddl.silver.sql
+++ scripts/silver/ddl.silver.sql
@@ -1,1 +1,9 @@
-
+/* 
+===========================================================================================================
+DDL Script: Creater Silver Tables
+===========================================================================================================
+Script Purpose:
+	This script creates tables in the 'silver' schema, dropping existing tables if they already exists.
+	Run this script to re-define the DDL structure of 'bronze' tables.
+===========================================================================================================
+*/
