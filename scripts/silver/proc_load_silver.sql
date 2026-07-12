c3c5e273570dd8331e8846273faf03494faf23b1
karlvaldezgit <karlvaldez49@yahoo.com>
2026-07-12 23:32:45 +0800

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,7 +1,14 @@
-
-
-
-
+/* 
+===========================================================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===========================================================================================================
+Script Purpose:
+	This stored procedure performs the ETL (Extract, Transform, Load) process to populate the 'silver' schema tables from the 'bronze' schema.
+Actions Performed:
+	- Truncates Silver tables.
+	- Inserts transformed and cleansed data from Bronze into Silver tables.
+===========================================================================================================
+*/
 
 
 
