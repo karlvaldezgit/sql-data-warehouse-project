3a1a8f408bea5ca27de26c9e5107e82b3d5247ea
karlvaldezgit <karlvaldez49@yahoo.com>
2026-07-12 23:31:21 +0800

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -9,3 +9,21 @@ Script Purpose:
   - Manually load data from csv files to bronze tables.
 ===========================================================================================================
 */
+
+
+--Deleting all table rows if exists before importing. Do this with all other files.
+TRUNCATE TABLE bronze.crm_cust_info;
+
+CSV files were imported using pgAdmin:
+1. Right-click the table
+2. Select Import/Export Data
+3. Choose the CSV file
+4. Set Format = CSV
+5. Set Header = Yes
+6. Click Import
+
+-- If encounters importing errors. Alter columns to text.
+ALTER TABLE bronze.crm_sales_details
+ALTER COLUMN sls_order_dt TYPE TEXT,
+ALTER COLUMN sls_ship_dt TYPE TEXT,
+ALTER COLUMN sls_due_dt TYPE TEXT;
