07ad1c2f447f805639d9b47497bc29eb2cdcaaf3
karlvaldezgit <karlvaldez49@yahoo.com>
2026-08-07 17:54:59 +0800

    Update 02_dimension_exploration.sql

--- scripts/data_analysis/02_dimension_exploration.sql
+++ scripts/data_analysis/02_dimension_exploration.sql
@@ -1,1 +1,8 @@
+-- Explore all countries our customers comes from.
+SELECT DISTINCT(country)
+FROM gold.dim_customers;
 
+-- Explore all categories 'The Major Divisions'
+SELECT DISTINCT(category), subcategory
+FROM gold.dim_products
+ORDER BY category;
