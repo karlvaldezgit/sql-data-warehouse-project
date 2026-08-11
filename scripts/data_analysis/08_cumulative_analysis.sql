53bc9ee1bfff30633e5a147186a43ab26cbb378b
karlvaldezgit <karlvaldez49@yahoo.com>
2026-08-11 21:27:21 +0800

    Update 08_cumulative_analysis.sql

--- scripts/data_analysis/08_cumulative_analysis.sql
+++ scripts/data_analysis/08_cumulative_analysis.sql
@@ -1,1 +1,13 @@
-
+-- Calculate the total sales per month and running total sales over time.
+SELECT 
+order_date,
+total_sales,
+SUM(total_sales) OVER (PARTITION BY order_date ORDER BY order_date) AS running_total_sales
+FROM(
+SELECT 
+DATE_TRUNC('month', order_date)::DATE AS order_date,
+SUM(sales_amount) AS total_sales,
+COUNT(DISTINCT customer_key) AS total_customers
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY 1);
