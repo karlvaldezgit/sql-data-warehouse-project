9b5c781a3db768a42ed93f54458f6228d8985237
karlvaldezgit <karlvaldez49@yahoo.com>
2026-08-17 23:26:18 +0800

    Update 10_part_to_whole_analysis.sql

--- scripts/data_analysis/10_part_to_whole_analysis.sql
+++ scripts/data_analysis/10_part_to_whole_analysis.sql
@@ -1,1 +1,18 @@
+-- Which categories contribute the most to overall sales
+WITH category_sales AS (
+SELECT
+category,
+SUM(sales_amount) AS total_sales
+FROM gold.fact_sales AS f
+LEFT JOIN gold.dim_products AS p
+ON f.product_key = p.product_key
+GROUP BY category
+)
 
+SELECT 
+category, 
+total_sales,
+SUM(total_sales) OVER() AS overall_saless,
+CONCAT(ROUND((total_sales/SUM(total_sales) OVER())*100,2), '%') AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC
